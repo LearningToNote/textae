bc8a71a7cecfbba2eb65121b9cb6437297705a04
Jan Philipp Sachse <jan.sachse@student.hpi.de>
2015-12-18 18:14:31 +0100

    Add GroupedTypeContainer

---
+++ src/lib/editor/GroupedTypeContainer/EntityType.js
@@ -0,0 +1,35 @@
+
+var EntityType = function(data) {
+    var name = data["name"],
+        code = data["code"],
+        group = data["group"],
+        groupId = data["groupId"]
+    return {
+        getName: function() {
+            return name
+        },
+        getCode: function() {
+            return code
+        },
+        getGroup: function() {
+            return group
+        },
+        getGroupId: function() {
+            return groupId
+        },
+        default: function() {
+            return false
+        },
+        toString: function() {
+            return code
+        }
+    }
+}
+
+EntityType.prototype.toString = function() {
+    return $(this).code
+};
+
+module.exports = function(data) {
+    return EntityType(data)
+}
---
+++ src/lib/editor/GroupedTypeContainer/index.js
@@ -0,0 +1,98 @@
+import reduce2hash from '../../util/reduce2hash'
+import uri from '../uri'
+import EntityType from './EntityType.js'
+
+var DEFAULT_TYPE = EntityType({"name": "blubb", "code": "000", "groupID": "000", "group": "No Group"}),
+  TypeContainer = function(getActualTypesFunction, defaultColor) {
+    var definedTypes = {},
+      defaultType = DEFAULT_TYPE
+
+    return {
+      setDefinedTypes: function(newDefinedTypes) {
+        definedTypes = newDefinedTypes
+      },
+      getDeinedTypes: function() {
+        return _.extend({}, definedTypes)
+      },
+      setDefaultType: function(type) {
+        defaultType = type
+      },
+      getDefaultType: function() {
+        return defaultType || this.getSortedNames()[0]
+      },
+      getColor: function(code) {
+        console.log("trying to get the color of " + code)
+        console.log(definedTypes)
+        console.log(definedTypes[code])
+        return definedTypes[code] && definedTypes[code].color || defaultColor
+      },
+      getUri: function(code) {
+        console.log("trying to get the uri of " + code)
+        console.log(code)
+        return definedTypes[code] && definedTypes[code].uri ||
+          uri.getUrlMatches(code) ? code : undefined
+      },
+      getSortedNames: function() {
+        if (getActualTypesFunction) {
+          var typeCount = getActualTypesFunction()
+            .concat(Object.keys(definedTypes))
+            .reduce(function(a, b) {
+              a[b] = a[b] ? a[b] + 1 : 1
+              return a
+            }, {})
+
+          // Sort by number of types, and by name if numbers are same.
+          var typeNames = Object.keys(typeCount)
+          typeNames.sort(function(a, b) {
+            var diff = typeCount[b] - typeCount[a]
+            return diff !== 0 ? diff :
+              a > b ? 1 :
+              b < a ? -1 :
+              0
+          })
+
+          return typeNames
+        } else {
+          return []
+        }
+      }
+    }
+  },
+  setContainerDefinedTypes = function(container, newDefinedTypes) {
+    console.log("Got new default types:")
+    console.log(newDefinedTypes)
+    // expected newDefinedTypes is an array of object. example of object is {"name": "Regulation","color": "#FFFF66","default": true}.
+    if (newDefinedTypes !== undefined) {
+      var test = createCodeMapping(newDefinedTypes.map(EntityType))
+      console.log("###############################################")
+      console.log(test)
+      container.setDefinedTypes(test)
+    }
+    console.log("Newly defined types:")
+    console.log(container.getDeinedTypes())
+  }
+
+function createCodeMapping(entities) {
+  return entities.reduce(function(obj, element) {
+    obj[element.toString()] = element
+    return obj
+  }, {})
+}
+
+module.exports = function(model) {
+  var entityContainer = _.extend(new TypeContainer(model.annotationData.entity.types, '#77DDDD'), {
+      isBlock: function(type) {
+        // console.log(type, entityContainer.getDeinedTypes(), entityContainer.getDeinedTypes()[type]);
+        var definition = entityContainer.getDeinedTypes()[type]
+        return definition && definition.type && definition.type === 'block'
+      }
+    }),
+    relationContaier = new TypeContainer(model.annotationData.relation.types, '#555555')
+
+  return {
+    entity: entityContainer,
+    setDefinedEntityTypes: _.partial(setContainerDefinedTypes, entityContainer),
+    relation: relationContaier,
+    setDefinedRelationTypes: _.partial(setContainerDefinedTypes, relationContaier)
+  }
+}
